58c5688d90d4191db405a444e1e9221a0609ff57
Ynothari <150809545+Ynothari@users.noreply.github.com>
2024-08-08 09:44:20 +0530

    Update atm1.py

--- atm1.py
+++ atm1.py
@@ -5,7 +5,7 @@ import base64
 import io
 
 # MongoDB connection
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb+srv://Harikrishnan:hari%40123@hari.uxntd.mongodb.net/')
 db = client['atm']
 users_collection = db['users']
 transactions_collection = db['transactions']
